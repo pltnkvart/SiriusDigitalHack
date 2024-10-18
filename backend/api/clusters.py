ffc4abafe5cb7b42c0c0ea88f71089fe5199562e
transaero21 <transaero21@elseboot.ru>
2024-10-19 01:22:21 +0300

    fix: remove file_id from clusters endpoint

--- backend/api/clusters.py
+++ backend/api/clusters.py
@@ -7,11 +7,10 @@ bp = Blueprint('clusters', __name__)
 
 
 @bp.route('/clusters', methods=['GET'])
-def download_file(file_id):
-
+def generate_clusters():
     try:
         session_id = session.get('upload_id')
-        file_path = get_file_for_session(session_id, file_id)
+        file_path = get_file_for_session(session_id)
         questions_clusters_array = make_clusterization(file_path)
 
         for question in questions_clusters_array:
