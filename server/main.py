76d25428fad7bd0096cf5f14226ff1f17e4d8aef
transaero21 <transaero21@elseboot.ru>
2024-10-18 15:58:39 +0300

    feat: introduce server to manage exit surveys

---
+++ server/main.py
@@ -0,0 +1,29 @@
+from flask import Flask
+import atexit
+import os
+
+from server.api import upload, download
+
+app = Flask(__name__)
+app.json.sort_keys = False
+app.secret_key = os.urandom(24)
+
+temporary_files = {}
+
+
+def cleanup():
+    for temp_files in temporary_files.values():
+        for temp_file in temp_files:
+            try:
+                os.remove(temp_file)
+            except OSError:
+                print(f"Error removing temporary file: {temp_file}")
+
+
+atexit.register(cleanup)
+
+app.register_blueprint(upload.bp)
+app.register_blueprint(download.bp)
+
+if __name__ == '__main__':
+    app.run(debug=True)
