0f51782628a3a4b951c5be9be5f986dc62f09702
hlebnoe_pole <glebpavlyuk@gmail.com>
2024-10-19 05:14:29 +0300

    Begin of Clusters by group idx

--- backend/api/clusters.py
+++ backend/api/clusters.py
@@ -1,7 +1,14 @@
+import json
+
 from flask import Blueprint, jsonify, session
+import tempfile
+import pickle
 
 from utils.clusters import make_clusterization
 from utils.session_manager import get_file_for_session
+
+from backend.utils.clusters import make_clusterization_for_group
+from backend.utils.session_manager import add_file_by_session_and_group
 
 bp = Blueprint('clusters', __name__)
 
@@ -13,14 +20,40 @@ def generate_clusters():
         file_path = get_file_for_session(session_id)
         questions_clusters_array = make_clusterization(file_path)
 
-        for question in questions_clusters_array:
-            print(f"new question")
-            for cluster in question:
-                print("new cluster")
-                for answer in range(min(len(cluster), 2)):
-                    print(cluster[answer])
+        with open('data.pickle', 'wb') as f:
+            pickle.dump(questions_clusters_array, f)
+
 
     except ValueError as e:
         return jsonify({'error': str(e)}), 400
 
     return jsonify(questions_clusters_array), 200
+
+@bp.route('/clusters/<group_id>', methods=['POST'])
+def get_group_clusters(group_id):
+
+    try:
+
+        session_id = session.get('upload_id')
+        file_path = get_file_for_session(session_id)
+
+        with open(file_path, 'r', encoding='utf-8') as file:
+            data = json.load(file)
+
+        questions_idxes = []
+        for group in data['groups']:
+            if group['id'] == group_id:
+                questions_idxes = group['question_ids']
+                break
+
+        questions = []
+        for question in data['questions']:
+            if question['id'] in questions_idxes:
+                questions.append(question)
+
+        questions_clusters_array = make_clusterization_for_group(questions)
+        # todo: save questions_clusters_array to pickle
+        # add_file_by_session_and_group(session_id, group_id, <paste here path>)
+
+    except ValueError as e:
+        return jsonify({'error': str(e)}), 400
--- backend/utils/session_manager.py
+++ backend/utils/session_manager.py
@@ -18,10 +18,26 @@ def add_file_to_session(session_id: str, file_path: str):
         raise ValueError("Invalid session ID")
 
 
+def add_file_by_session_and_group(session_id: str, group_idx: str, file_path: str):
+    if session_id in temporary_files:
+        temporary_files[session_id + '/' + group_idx] = file_path
+    else:
+        raise ValueError("Invalid session ID")
+
+
 def get_file_for_session(session_id: str) -> str:
     if session_id in temporary_files:
         if temporary_files[session_id] is not None:
             return temporary_files[session_id]
+        else:
+            raise ValueError("No file found")
+    else:
+        raise ValueError("Invalid session ID")
+
+def get_file_for_session_and_group(session_id: str, group_idx: str) -> str:
+    if session_id + '/' + group_idx in temporary_files:
+        if temporary_files[session_id + '/' + group_idx] is not None:
+            return temporary_files[session_id + '/' + group_idx]
         else:
             raise ValueError("No file found")
     else:
